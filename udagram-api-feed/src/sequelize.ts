cece61434ed1cbb8cdbe85a911629482be1eada9
VietPhu Nguyen <40367033+vietphuvn@users.noreply.github.com>
2024-12-30 15:28:10 +0800

    Update sequelize.ts

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -2,7 +2,7 @@ import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 
 
-export const sequelize = new Sequelize({
+export const sequelize = new Sequelize({ 
   'username': config.username,
   'password': config.password,
   'database': config.database,
